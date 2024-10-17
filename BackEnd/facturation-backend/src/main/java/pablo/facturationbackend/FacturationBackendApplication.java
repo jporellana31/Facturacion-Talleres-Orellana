692c3f3c2d1c842f9dc36204db9be1a6e0b02865
jporellana31 <juan.orellana.57@est.ucacue.edu.ec>
2024-10-17 18:07:07 -0500

    Inicialización del proyecto con backend y frontend

---
+++ BackEnd/facturation-backend/src/main/java/pablo/facturationbackend/FacturationBackendApplication.java
@@ -0,0 +1,13 @@
+package pablo.facturationbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturationBackendApplication.class, args);
+	}
+
+}
